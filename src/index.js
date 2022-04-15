c919890f1ff1eb7ca45a460974124b9e1f2ef7cb
Nicolás Vaca Corredor <cow@iMac-de-Nicolas.local>
2022-04-15 16:57:33 -0500

    add database configuration and mongoose

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 app.get('/', (req, res) => {
     res.send('Hello for all people!')
 })
+
+mongoose
+    .connect(process.env.MONGOBD_URL)
+    .then(() => console.log('Succesfully connection DB'))
+    .catch((error) => console.log('Error', error))
 
 app.listen(port, () => {
     console.log('App running now in port: ', port)
