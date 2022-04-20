ab0db8077c52067c0526061ecde1865d5aaf78c3
AndersonVa <andersonj.urbinav@konradlorenz.edu.co>
2022-04-20 02:52:00 -0500

    feat: Add assigned appointments endpoints

--- src/models/appointments/Appointment.model.js
+++ src/models/appointments/Appointment.model.js
@@ -1,31 +1,34 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const AppointmentSchema = mongoose.Schema({
-    
-    serviceType: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: String,
-        required: true
-    },
-    hour: {
-        type: String,
-        required: true
-    },
-    orderMedicines: {
-        type: String
-    },
-    active: {
-        type: Boolean, 
-        default: true
-    },
-    user: {
-        type: mongoose.Types.ObjectId,
-        ref: 'User',
-        required: true
-    }
-})
+  serviceTypeID: {
+    type: String,
+    required: true,
+  },
+  serviceType: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  hour: {
+    type: String,
+    required: true,
+  },
+  orderMedicines: {
+    type: String,
+  },
+  active: {
+    type: Boolean,
+    default: true,
+  },
+  user: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
 
-module.exports = mongoose.model('Appointment', AppointmentSchema)
+module.exports = mongoose.model("Appointment", AppointmentSchema);
